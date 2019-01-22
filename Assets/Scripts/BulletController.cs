04b7dc32ff9f3deb7b8f13f62c5592da44e0efda
Mariusz <maniekb12@gmail.com>
2019-01-22 18:04:17 +0100

    added assets from asset store and improve mechanics

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,23 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
 	public string allyTag;
 	public int damage;
 	public uint destroyAfter;
+	public GameObject explosion;
+
 	void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.tag != allyTag)
 		{
 			Destroy(this.gameObject);
 		}
-		
+
 	}
 
 	private void Awake()
 	{
 		Destroy(this.gameObject, destroyAfter);
+	}
+
+	private void OnDestroy()
+	{
+		if (explosion)
+		{
+			Instantiate(explosion, this.transform.position, Quaternion.identity);
+		}
+
 	}
 }
